e3276e558da6610ca53446db7a10f2bb18363eb0
Alexander Kapshuna <kapsh@kap.sh>
2019-09-11 22:45:57 +0300

    dbeaver-community-bin: version bump to 6.2.0
    Add GTK+ overlay workaround from Archlinux.

---
+++ packages/dev-db/dbeaver-community-bin/files/dbeaver.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Disable GTK+ Overlay scrolling
+# https://bugs.archlinux.org/task/63338
+# https://bugs.eclipse.org/bugs/show_bug.cgi?id=519728
+
+export GTK_OVERLAY_SCROLLING=0
+exec "$(dirname "$(realpath "$0")")/dbeaver"
